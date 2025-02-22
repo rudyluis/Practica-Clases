d851a7c7d6f17dd485ff82b03fe8df3714f4fc85
rudyluis <rudy.manzaneda@gmail.com>
2025-02-22 11:39:12 -0400

    22052025 cambios de la clase del sabado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,20 @@ def index():
 @app.route("/bienvenida")
 def bienvenida():
     return render_template('bienvenida.html')
+
+@app.route("/usuario/<name>")
+def user(name):
+    return f"<h1>hola {name}</h1>"
+
+@app.route("/direccion")
+def redireccionar():
+    return redirect("http://www.univalle.edu")
+
+@app.route('/buscar')
+def buscar():
+    query = request.args.get('q', 'Nada encontrado')
+    return f"Buscar: {query}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
